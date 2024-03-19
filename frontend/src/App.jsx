32ca37e5952421ad8b9a3071ceeaa61af8b2a575
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-03-19 11:14:23 +0100

    router añadido

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import Router from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <h1>HOLA SOCIOS QUE PAGÁIS Y NO VENÍS NUNCA</h1>
-    </>
+      <Link to="/">HomePage</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/nombresala">Detalle Sala</Link>  
+      <Link to="/reservas">Pagina reserva</Link>
+      <Link to="/horario">Horario</Link>
+      <Link to="/misreservas">Mis Reservas</Link>
+    </Router>
   );
 }
 
