2adf7ae07aafd833d80e9399204a3ca839a01a78
Johnny <jonathanbc2012@gmail.com>
2024-03-20 11:50:37 +0100

    fix: change class in card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,13 +2,13 @@ import "./Card.css";
 
 export const Card = ({title, imageUrl}) => {
   return (
-    <div class="card-container">
+    <div className="card-container">
       <img
         src={imageUrl}
         alt={"imágen de " + title}
         class="card-img"
       />
-      <div class="card-text">
+      <div className="card-text">
         <div className="background"></div>
         <p>{title}</p>
       </div>
