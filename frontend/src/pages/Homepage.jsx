8fe0585580f3148cf05356b437429a40ebdee3b9
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-03-20 09:59:27 +0100

    Bug errors

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,1 +1,12 @@
+import HeaderGuest from "../components/HeaderGuest"
+
+const Homepage = () => {
+  return (<main>
+    <HeaderGuest />
+    <div>Homepage</div>
+  </main>
+  )
+}
+
+export default Homepage;
 
