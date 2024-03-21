8319ace43e71b305cb18a25220f8adbabbbcab8c
Johnny <jonathanbc2012@gmail.com>
2024-03-21 11:42:22 +0100

    login created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,64 +1,66 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
-import HeaderGuest from '../components/HeaderGuest';
+import HeaderGuest from "../components/HeaderGuest";
 
 const Login = () => {
-    //State for email and password
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  //State for email and password
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    
-       const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //Handle form submission
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  //Handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        try{
-            const loginSuccess = await Login({ email: email, password: password });
-            if (loginSuccess) {
-                setEmail('');
-                setPassword('');
-                navigate("/", { state: { msg: "Inicio de sesión correcto" } });
-            }
-        } 
-        catch (err) {
-            console.log(err);
-        }
-    };
-    
-    return (
-        
-        <div>
-        <HeaderGuest />
-        
-        <form onSubmit={handleSubmit} className="form">
+    try {
+      const loginSuccess = await Login({ email: email, password: password });
+      if (loginSuccess) {
+        setEmail("");
+        setPassword("");
+        navigate("/", { state: { msg: "Inicio de sesión correcto" } });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-            <label htmlFor="email">Correo electrónico</label>
-            <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                />
+  return (
+    <div>
+      <HeaderGuest />
 
-            <label htmlFor="password">Contraseña</label>
-            <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                />
-               <button className="form__btn">INICIAR SESIÓN </button>
-                
-        </form>
+      <form onSubmit={handleSubmit} className="form">
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <label htmlFor="password">Contraseña</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button className="form__btn">INICIAR SESIÓN </button>
+        <div class="form__opciones">
+          <label for="recordar" class="form__recordarme">
+            <input type="checkbox" id="recordar" />
+            Recordarme en este equipo
+          </label>
+          <a href="#" class="form__enlace">
+            ¿Olvidaste tu contraseña?
+          </a>
+        </div>
+      </form>
     </div>
-    
-    );
+  );
 };
-
 
 export default Login;
