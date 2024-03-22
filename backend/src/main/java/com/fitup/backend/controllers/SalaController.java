f0668d849a9761a5d9d51d1dc683eeaa8c046e95
Anadevega74 <151641554+Anadevega74@users.noreply.github.com>
2024-03-22 12:40:23 +0100

    Sala finished

--- backend/src/main/java/com/fitup/backend/controllers/SalaController.java
+++
@@ -1,5 +0,0 @@
-package com.fitup.backend.controllers;
-
-public class SalaController {
-    
-}
--- backend/src/main/java/com/fitup/backend/services/SalaService.java
+++ backend/src/main/java/com/fitup/backend/services/SalaService.java
@@ -1,5 +1,24 @@
 package com.fitup.backend.services;
 
+
+import org.springframework.stereotype.Service;
+
+import com.fitup.backend.models.Sala;
+import com.fitup.backend.persistence.Sala.SalaRepository;
+import java.util.List;
+
+@Service
 public class SalaService {
+    private final SalaRepository salaRepository;
+
     
+    public SalaService(SalaRepository salaRepository) {
+        this.salaRepository = salaRepository;
+    }
+
+    public List<Sala> obtenerSala() {
+        return salaRepository.findAll();
+
+    }
+
 }
